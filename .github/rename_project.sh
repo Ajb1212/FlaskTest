d770643428873253ea9fc0833b7e524613256fb5
Ajb1212 <Ajb1212@users.noreply.github.com>
2023-01-29 11:14:42 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="Ajb1212"
+original_name="flasktest"
+original_urlname="FlaskTest"
+original_description="Awesome flasktest created by Ajb1212"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv flasktest $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
